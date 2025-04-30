197965a7cad0371b4152142e6a5f4bb6632d92c5
Salman Sayeed <ss.salmansayeed@gmail.com>
2025-04-30 15:16:13 +0600

    contact us

---
+++ Salman Sayeed/Contacts Page/script.js
@@ -0,0 +1,16 @@
+let fnameVal = ()=>{
+    const fName = document.getElementById('fname').value.trim();
+}
+let lnameVal = ()=>{
+
+}
+let emailVal = ()=>{
+
+}
+let messageVal = ()=>{
+
+}
+
+let phoneVal = () =>{
+    return(fnameVal());
+}
