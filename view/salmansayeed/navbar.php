6b39d5c9ac506a3266cc58de4ec1bf712f649613
Salman Sayeed <ss.salmansayeed@gmail.com>
2025-05-11 21:48:43 +0600

    revamped release  calander

--- view/salmansayeed/navbar.php
+++ view/salmansayeed/navbar.php
@@ -7,11 +7,11 @@
                  <a class="login-container-link btn" href="#loginpage">Login</a>      
             </div>
         </li>
-        <li class="sidebar-list-item">Home</li>
+        <li class="sidebar-list-item" onclick="window.location.href='../../index.php'">Home</li>
         <li class="sidebar-list-item">Movies</li>
         <li class="sidebar-list-item">Series</li>
         <li class="sidebar-list-item">Country</li>
-        <li class="sidebar-list-item">Country</li>
+        <li class="sidebar-list-item" onclick="window.location.href='releasecalander.php'">Calander</li>
     </ul>
 </div>
 
@@ -26,11 +26,11 @@
         </div>
         <div class="menu-container">
             <ul class="menu-list">
-                <li class="menu-list-item hideOnMobile">Home</li>
+                <li class="menu-list-item hideOnMobile" onclick="window.location.href='../../index.php'">Home</li>
                 <li class="menu-list-item hideOnMobile">Movies</li>
                 <li class="menu-list-item hideOnMobile">Series</li>
                 <li class="menu-list-item hideOnMobile">Country</li>
-                <li class="menu-list-item hideOnMobile">Country</li>
+                <li class="menu-list-item hideOnMobile" onclick="window.location.href='releasecalander.php'">Calander</li>
             </ul>
         </div>
         <div class="login-container ">
@@ -43,5 +43,8 @@
                 <div class="toggle-ball"></div>
             </div>
         </div>
+
+        <script src="../../assets/salmansayeed/main-script.js"></script>
+        <script src="../../assets/salmansayeed/releasecalander/script-releasecalander.js"></script>
     </div>  
 </div>
---
+++ view/salmansayeed/releasecalander.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Screenverse | Calander</title>
+
+    <link rel="icon" type="image/png" href="../../assets/icons/logosmall.png">
+
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css">
+
+    <link rel="stylesheet" href="../../assets/salmansayeed/main-style.css">
+    <link rel="stylesheet" href="../../assets/salmansayeed/releasecalander/style-releasecalander.css">
+</head>
+<body>
+    <?php include('navbar.php') ?>
+
+    <div class="continer-content">   </div>
+
+    <?php include('footer.php') ?>
+</body>
+</html>
